f90f4aff881dece57af17ab9a2c2088524852a59
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2026-04-19 18:08:33 +0100

    commit

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,26 +1,26 @@
 export default function sitemap() {
-    const baseUrl = 'https://greenball360.com';
-  
-    return [
-      {
-        url: `${baseUrl}/`,
-        lastModified: new Date(),
-      },
-      {
-        url: `${baseUrl}/about`,
-        lastModified: new Date(),
-      },
-      {
-        url: `${baseUrl}/contact`,
-        lastModified: new Date(),
-      },
-      {
-        url: `${baseUrl}/news`,
-        lastModified: new Date(),
-      },
-      {
-        url: `${baseUrl}/leaderboard`,
-        lastModified: new Date(),
-      },
-    ];
-  }
+  const baseUrl = 'https://www.greenball360.com';
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/news`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/leaderboard`,
+      lastModified: new Date(),
+    },
+  ];
+}
